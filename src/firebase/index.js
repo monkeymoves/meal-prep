be1b55c134c1d7f1ba4d937b68f78ef92a9158e4
monkeymoves <lukejjmaggs@gmail.com>
2019-05-06 00:16:58 +0100

    Luke : Meal prep app, first commit - firebase working but test db (need to secure)

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyC_a2zswEdvwmIMG4iX-2qYQtGY5THS8Cw",
+    authDomain: "meal-prep-7305a.firebaseapp.com",
+    databaseURL: "https://meal-prep-7305a.firebaseio.com",
+    projectId: "meal-prep-7305a",
+    storageBucket: "meal-prep-7305a.appspot.com",
+    messagingSenderId: "296638170148",
+    appId: "1:296638170148:web:cfdd096932ab9019"
+};
+firebase.initializeApp(firebaseConfig);
